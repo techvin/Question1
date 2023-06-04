d931bc6dcd1878f3a4690b17257b4d84ba5940e4
techvin <vsb140592@gmail.com>
2023-06-04 13:06:18 +0530

    formatted code

--- GraphQuestion1.java
+++ GraphQuestion1.java
@@ -5,49 +5,48 @@ import java.util.Arrays;
 import java.util.List;
 
 //This class is used for setting source and destination vertices for a given edge
-class Edge
-{
-    int source, dest;
- 
-    public Edge(int source, int dest)
-    {
-        this.source = source;
-        this.dest = dest;
-    }
+class Edge {
+	int source, dest;
+
+	public Edge(int source, int dest) {
+		this.source = source;
+		this.dest = dest;
+	}
 }
 
 //This class is used to create a graph
-class Graph implements GNode
-{
+class Graph implements GNode {
 	String name;
 	GNode[] childrenArray;
-	
-    List<List<Integer>> adjList = null;
-    Graph(List<Edge> edges, int n)
-    {
-        adjList = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            adjList.add(new ArrayList<>());
-        }
-        for (Edge edge: edges) {
-            adjList.get(edge.source).add(edge.dest);
-        }
-    }
-    Graph() {};
-    
-    public List<List<Integer>> getAdjacencyList() {
-        return adjList;
-    }
-    
-    public void printAdjacencyList() {
-        for (int i = 0; i < adjList.size(); i++) {
-            System.out.print("Adjacency list of " + i + " = ");
-            for (int j = 0; j < adjList.get(i).size(); j++) {
-                System.out.print(adjList.get(i).get(j) + " ");
-            }
-            System.out.println();
-        }
-    }
+
+	List<List<Integer>> adjList = null;
+
+	Graph(List<Edge> edges, int n) {
+		adjList = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			adjList.add(new ArrayList<>());
+		}
+		for (Edge edge : edges) {
+			adjList.get(edge.source).add(edge.dest);
+		}
+	}
+
+	Graph() {
+	};
+
+	public List<List<Integer>> getAdjacencyList() {
+		return adjList;
+	}
+
+	public void printAdjacencyList() {
+		for (int i = 0; i < adjList.size(); i++) {
+			System.out.print("Adjacency list of " + i + " = ");
+			for (int j = 0; j < adjList.get(i).size(); j++) {
+				System.out.print(adjList.get(i).get(j) + " ");
+			}
+			System.out.println();
+		}
+	}
 
 	@Override
 	public String getName() {
@@ -68,8 +67,9 @@ class Graph implements GNode
 	public void setChildren(GNode[] childrenArray) {
 		this.childrenArray = childrenArray;
 	}
-	
-	// this function accepts GNode vertex as a parameter and returns all the vertices of a graph
+
+	// this function accepts GNode vertex as a parameter and returns all the
+	// vertices of a graph
 	@SuppressWarnings("rawtypes")
 	@Override
 	public ArrayList walkGraph(GNode graph) {
@@ -90,31 +90,28 @@ class Graph implements GNode
 		}
 		return vertexList;
 	}
-    
+
 }
- 
-class GraphQuestion1
-{	
-    @SuppressWarnings("unchecked")
+
+class GraphQuestion1 {
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-    	List<Edge> edges = Arrays.asList(
-                new Edge(0, 1), new Edge(1, 2), new Edge(1, 3),
-                new Edge(2, 3), new Edge(3, 4)
-        );
-    	
-        int n = 5;
-        GNode graph = new Graph(edges, n);
-        graph.printAdjacencyList();
-        System.out.println();
-        System.out.println("=========");
-        System.out.println("\nAll vertices of a graph are = " + graph.walkGraph(graph) + "\n");
-        ArrayList<GNode> vertexList = graph.walkGraph(graph);
-        System.out.println("=========");
-        for (GNode vertex: vertexList) {
-        	System.out.println();
-        	System.out.print("Vertex name : " + vertex.getName());
-        	System.out.print(" | Vertex adjacent vertices array size : " + vertex.getChildren().length);
-        	System.out.print("| Vertex adjacent vertices array : " + vertex.getChildren());
-        }
-    }
+		List<Edge> edges = Arrays.asList(new Edge(0, 1), new Edge(1, 2), new Edge(1, 3), new Edge(2, 3),
+				new Edge(3, 4));
+
+		int n = 5;
+		GNode graph = new Graph(edges, n);
+		graph.printAdjacencyList();
+		System.out.println();
+		System.out.println("=========");
+		System.out.println("\nAll vertices of a graph are = " + graph.walkGraph(graph) + "\n");
+		ArrayList<GNode> vertexList = graph.walkGraph(graph);
+		System.out.println("=========");
+		for (GNode vertex : vertexList) {
+			System.out.println();
+			System.out.print("Vertex name : " + vertex.getName());
+			System.out.print(" | Vertex adjacent vertices array size : " + vertex.getChildren().length);
+			System.out.print("| Vertex adjacent vertices array : " + vertex.getChildren());
+		}
+	}
 }
